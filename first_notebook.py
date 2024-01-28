a80efc4ee44d46a6c9fbdf28680c5370f8b7f113
daniellee-webstep <daniellee-webstep>
2024-01-28 13:50:15 +0000

    first commit
    created first_notebook

---
+++ first_notebook.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+# Import the necessary module
+import module_name
+
+# Define a function named 'function_name' that accepts two parameters: 'param1' and 'param2'
+def function_name(param1, param2):
+    # Perform some action with 'param1'
+    result = module_name.action(param1)
+    
+    # Check if the result is equal to 'param2'
+    if result == param2:
+        # Print a message indicating that the result is equal to 'param2'
+        print("Result is equal to param2")
+    else:
+        # Print a message indicating that the result is not equal to 'param2'
+        print("Result is not equal to param2")
+    
+# Call the function 'function_name' with arguments 'arg1' and 'arg2'
+function_name(arg1, arg2)
+
